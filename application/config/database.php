132b18b8feaee30f2b68dba7982e1b39cfc5ff66
santiyunikas <santiyunikas@gmail.com>
2020-09-28 20:27:27 +0700

    try use real server

--- application/config/database.php
+++ application/config/database.php
@@ -73,11 +73,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'ublearni_mathgeo',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => 'mathgeo.ub-learningtechnology.com',
+	'username' => 'mathgeo_root',
+	'password' => 'mathgeo_root',
 	'database' => 'ublearni_mathgeo',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
@@ -94,3 +116,4 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+?>
--- application/controllers/C_Member.php
+++ application/controllers/C_Member.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
-class C_Pengguna extends REST_Controller {
+class C_Member extends REST_Controller {
 
     function __construct($config = 'rest') {
         parent::__construct($config);
@@ -14,26 +14,26 @@ class C_Pengguna extends REST_Controller {
 
     //Menampilkan data kontak
     function index_get() {
-        $id = $this->get('id_pengguna');
+        $id = $this->get('id_member');
         if ($id == '') {
-            $pengguna = $this->db->get('pengguna')->result();
+            $member = $this->db->get('member')->result();
         } else {
-            $this->db->where('id_pengguna', $id);
-            $pengguna = $this->db->get('pengguna')->result();
+            $this->db->where('id_member', $id);
+            $member = $this->db->get('member')->result();
         }
-        $this->response($pengguna, 200);
+        $this->response($member, 200);
     }
 
     //Mengirim atau menambah data kontak baru
     function index_post() {
         $data = array(
-            'id_pengguna'=>$this->post('id_pengguna'),
+            'id_member'=>$this->post('id_member'),
             'email'=> $this->post('email'),
             'password'=> $this->post('password'),
             'nama_lengkap'=> $this->post('nama_lengkap'),
             'nomor_telepon'=> $this->post('nomor_telepon')
         );
-        $insert = $this->db->insert('pengguna', $data);
+        $insert = $this->db->insert('member', $data);
         if ($insert) {
             $this->response($data, 200);
         } else {
@@ -43,27 +43,28 @@ class C_Pengguna extends REST_Controller {
 
     //Memperbaharui data yang telah ada
     function index_put() {
-        $id = $this->put('id_pengguna');
+        $id = $this->put('id_member');
         $data = array(
-            'id_pengguna'=>$this->put('id_pengguna'),
+            'id_member'=>$this->put('id_member'),
             'email'=> $this->put('email'),
             'password'=> $this->put('password'),
             'nama_lengkap'=> $this->put('nama_lengkap'),
             'nomor_telepon'=> $this->put('nomor_telepon')
                 );
-        $this->db->where('id_pengguna', $id);
-        $update = $this->db->update('pengguna', $data);
+        $this->db->where('id_member', $id);
+        $update = $this->db->update('member', $data);
         if ($update) {
             $this->response($data, 200);
         } else {
             $this->response(array('status' => 'fail', 502));
         }
     }
-
+    
+    //delete data yang sudah ada
     function index_delete() {
-        $id = $this->delete('id_pengguna');
-        $this->db->where('id_pengguna', $id);
-        $delete = $this->db->delete('pengguna');
+        $id = $this->delete('id_member');
+        $this->db->where('id_member', $id);
+        $delete = $this->db->delete('member');
         if ($delete) {
             $this->response(array('status' => 'success'), 201);
         } else {
