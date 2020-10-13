0d5ca11082128538843748c3a3d8189ba3305df0
santiyunikas <santiyunikas@gmail.com>
2020-10-13 21:45:23 +0700

    Update C_Register.php

--- application/controllers/C_Register.php
+++ application/controllers/C_Register.php
@@ -56,7 +56,7 @@ class C_Register extends REST_Controller {
                 <p>Password: '.$data['password'].'</p>
                 <p>-----------------------------------</p>
                 <p>Klik link dibawah ini untuk mengaktifkan akun kamu:</p>
-                https://mathgeo.ub-learningtechnology.com/index.php/C_Register/index_put/'.$data['id_pengguna'].'/'.$data['email'].'/'.$data['password'].'/'.$data['nama_lengkap'].'/'.$data['nomor_telepon'].'/1
+                https://mathgeo.ub-learningtechnology.com/index.php/C_Register/index_put/'.$data['id_pengguna'].'/'.$data['email'].'/1
                 <p>Selamat belajar dengan tekun</p>                
                 <p>Salam,</p>
                 <p><strong>Santi</strong></p>
@@ -134,7 +134,7 @@ class C_Register extends REST_Controller {
     }
 
     //Memperbaharui data yang telah ada
-    function index_put($id, $email, $password, $nama_lengkap, $nomor_telepon, $active) {
+    function index_put($id, $email, $active) {
         // $id = $this->put('id_pengguna');
         $data = array(
           'id_pengguna'=>$id,
