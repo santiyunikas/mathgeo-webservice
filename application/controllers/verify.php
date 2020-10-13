ea56031326981d0148d202b1bbcb73b08df8feca
santiyunikas <santiyunikas@gmail.com>
2020-10-13 14:10:30 +0700

    Update verify.php

--- application/controllers/verify.php
+++ application/controllers/verify.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 use Restserver\Libraries\REST_Controller;
 
-class verify extends REST_Controller {
+class Verify extends REST_Controller {
 
     function __construct($config = 'rest') {
         parent::__construct($config);
