7f4a8c70b8c75f676e8b8a007781a037c7d78435
santiyunikas <santiyunikas@gmail.com>
2020-10-13 12:48:54 +0700

    Update verify.php

--- application/controllers/verify.php
+++ application/controllers/verify.php
@@ -1,26 +1,48 @@
 <?php
-$db = \Config\Database::connect();
-$db = db_connect();
 
-if(isset($_GET['email'])){
-    // Verify data
-    $email = $_GET['email']; // Set email variable
-                 
-    $query = $db->query('YOUR QUERY HERE');
-    $search = mysql_query("SELECT email FROM is_verified_member WHERE email='".$email) or die(mysql_error()); 
-    $match  = mysql_num_rows($search);
-                  
-    if($match > 0){
-        // We have a match, activate the account
-        mysql_query("UPDATE is_verified_member SET active='1' WHERE email='".$email."'AND active='0'") or die(mysql_error());
-        echo '<div class="statusmsg">Your account has been activated, you can now login</div>';
-    }else{
-        // No match -> invalid url or account has already been activated.
-        echo '<div class="statusmsg">The url is either invalid or you already have activated your account.</div>';
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . '/libraries/email/PHPMailer/PHPMailerAutoload.php';
+use Restserver\Libraries\REST_Controller;
+
+class C_Register extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
     }
-                  
-}else{
-    // Invalid approach
-    echo '<div class="statusmsg">Invalid approach, please use the link that has been send to your email.</div>';
+
+    //Menampilkan data menggunakan email
+    function index_get() {
+        $email = $this->get('email');
+        if ($email == '') {
+            $member = $this->db->get('')->result();
+        } else {
+            $this->db->where('email', $email);
+            $member = $this->db->get('member')->result();
+        }
+        $this->response($member, 200);
+
+        if(!empty($email)){
+            // Verify data                          
+            $search = mysql_query("SELECT email, active FROM is_verified_member WHERE email='".$email."' AND active='0'") or die(mysql_error()); 
+            $match  = mysql_num_rows($search);
+                          
+            if($match > 0){
+                // We have a match, activate the account
+                mysql_query("UPDATE is_verified_member SET active='1' WHERE email='".$email."' AND active='0'") or die(mysql_error());
+                echo '<div class="statusmsg">Your account has been activated, you can now login</div>';
+            }else{
+                // No match -> invalid url or account has already been activated.
+                echo '<div class="statusmsg">The url is either invalid or you already have activated your account.</div>';
+            }
+                          
+        }else{
+            // Invalid approach
+            echo '<div class="statusmsg">Invalid approach, please use the link that has been send to your email.</div>';
+        }
+    }
+
 }
 ?>
