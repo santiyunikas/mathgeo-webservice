0752549d8b0bbdb6da17513867ae03dc19496a85
santiyunikas <santiyunikas@gmail.com>
2020-10-13 08:53:17 +0700

    Update C_Register.php

--- application/controllers/C_Register.php
+++ application/controllers/C_Register.php
@@ -3,9 +3,8 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
-require APPPATH . '/libraries/function.php';
+require APPPATH . '/libraries/email/PHPMailer/PHPMailerAutoload.php';
 use Restserver\Libraries\REST_Controller;
-use Restserver\Libraries\Email\function;
 
 class C_Register extends REST_Controller {
 
@@ -73,6 +72,77 @@ class C_Register extends REST_Controller {
                 Balas email ini untuk informasi lebih lanjut.';
         smtp_mail($to, $subject, $message, '', '', 0, 0, true);
     }
+    
+    function smtp_mail($to, $subject, $message, $from_name, $from, $cc, $bcc, $debug=false) {
+        $mail = new PHPMailer;
+        $mail->SMTPDebug = $debug; // Ubah menjadi true jika ingin menampilkan sistem debug SMTP Mailer
+        $mail->isSMTP();
+        
+        // Hapus Semua Tujuan, CC dan BCC
+        $mail->ClearAddresses();  
+        $mail->ClearCCs();
+        $mail->ClearBCCs();
+      
+        /* -------------------------- Konfigurasi Dasar SMTP ---------------------------------- */
+        
+        $mail->SMTPAuth 	= true;                        				
+        $mail->Host 		  = 'mail.ub-learningtechnology.com';  // Masukkan Server SMTP
+        $mail->Port 		  = 587;                                      // Masukkan Port SMTP
+        $mail->SMTPSecure = 'tls';                                    // Masukkan Pilihan Enkripsi ( `tls` atau `ssl` )
+        $mail->Username 	= 'mathgeo_team@ub-learningtechnology.com';                // Masukkan Email yang digunakan selama proses pengiriman email via SMTP
+        $mail->Password 	= 'mathgeo_team';        						          // Masukkan Password dari Email tsb
+        $default_email_from       = 'mathgeo_team@ub-learningtechnology.com';        // Masukkan default from pada email
+        $default_email_from_name  = 'Santi dari MathGeo';           // Masukkan default nama dari from pada email
+        
+        /* -------------------------- Konfigurasi Dasar SMTP ---------------------------------- */
+        
+        if(empty($from)) $mail->From = $default_email_from;
+        else $mail->From = $from;
+      
+        if(empty($from_name)) $mail->FromName = $default_email_from_name;
+        else $mail->FromName = $from_name;
+        
+        // Set penerima email
+        if(is_array($to)) {
+          foreach($to as $k => $v) {
+            $mail->addAddress($v);
+          }
+        } else {
+          $mail->addAddress($to);
+        }
+        
+        // Set email CC ( optional )
+        if(!empty($cc)) {
+          if(is_array($cc)) {
+            foreach($cc as $k => $v) {
+              $mail->addCC($v);
+            }
+          } else {
+            $mail->addCC($cc);
+          }
+        }
+        
+        // Set email BCC ( optional )
+        if(!empty($bcc)) {
+          if(is_array($bcc)) {
+            foreach($bcc as $k => $v) {
+              $mail->addBCC($v);
+            }
+          } else {
+            $mail->addBCC($bcc);
+          }
+        }
+        
+        // Set isi dari email
+        $mail->isHTML(true);
+        $mail->Subject 	= $subject;
+        $mail->Body 	  = $message;
+        $mail->AltBody	= $message;
+        if(!$mail->send())
+          return 1;
+        else
+          return 0;
+      }
 
     //Memperbaharui data yang telah ada
     function index_put() {
