1c63d69fd3bc793be40b1143e8ca250caa2a465d
santiyunikas <santiyunikas@gmail.com>
2020-11-15 05:47:49 +0700

    update fitur quiz

---
+++ application/controllers/C_DetailQuiz.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class C_DetailQuiz extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+
+    //mengambil data menggunakan email
+    function index_get() {
+        $id_quiz = $this->get('id_quiz');
+        if ($id_quiz == '') {
+            $soal_quiz = $this->db->get('soal_quiz')->result();
+        } else {
+            $this->db->where('id_quiz', $id_quiz);
+            $soal_quiz = $this->db->get('soal_quiz')->result();
+        }
+        $this->response($soal_lquiz, 200);
+    }
+
+}
+?>
---
+++ application/controllers/C_StatusMengerjakanQuiz.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class C_StatusMengerjakanQuiz extends REST_Controller {
+
+    function __construct($config = 'rest') {
+        parent::__construct($config);
+        $this->load->database();
+    }
+
+    function index_get() {
+        $id_quiz = $this->get('id_quiz');
+        $id_member = $this->get('id_member');
+        if ($id_member == '') {
+            $reviewQuiz = $this->db->get('status_mengerjakan_quiz')->result();
+        } else {
+            $array = array('id_quiz =' => $id_quiz,
+                            'id_member =' => $id_member);
+            $this->db->where($array);
+            $reviewQuiz = $this->db->get('status_mengerjakan_quiz')->result();
+        }
+        $this->response($reviewQuiz, 200);
+    }
+
+    function index_post() {
+        $data = array(
+            'id_member'=>$this->post('id_member'),
+            'id_quiz'=> $this->post('id_quiz'),
+            'status_pengerjaan'=> 1,
+            'nilai'=> $this->post('nilai')
+        );
+        $insert = $this->db->insert('status_mengerjakan_quiz', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    function index_put() {
+        $data = array(
+            'id_member'=> $this->put('id_member'),
+            'id_quiz'=> $this->put('id_quiz')
+        );
+        $nilai = array(
+            'nilai'=> $this->put('nilai')
+        );
+
+        $this->db->where($data);
+        $update = $this->db->update('status_mengerjakan_quiz', $nilai);
+        if ($update) {
+            $this->response($nilai, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+}
+?>
